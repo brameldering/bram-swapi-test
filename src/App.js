09346e812715dfffc75ef512be426073282ba1f6
brameldering <bram.eldering@gmail.com>
2023-06-09 14:01:53 +0200

    Added Firebase API DB and Add Movie functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import LoadingIndicator from "./components/UI/LoadingIndicator";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -14,27 +15,33 @@ function App() {
     setError(null);
     console.log("fetchMoviesHandler");
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-1dd0d-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
       if (!response.ok) {
-        throw new Error("Error processing: " + response.status);
+        throw new Error("Error processing fetch request: " + response.status);
       }
 
       const data = await response.json();
+      console.log(data);
+      if (!data) {
+        throw new Error("Error processing response");
+      }
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-      setIsloading(false);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
-      setIsloading(false);
     }
+    setIsloading(false);
   }, []);
 
   useEffect(() => {
@@ -42,6 +49,34 @@ function App() {
 
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    console.log("addMoviesHandler");
+    console.log(movie);
+
+    try {
+      const response = await fetch(
+        "https://react-http-1dd0d-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Error processing post request: " + response.status);
+      }
+      const data = await response.json();
+      console.log(data);
+      if (!data) {
+        throw new Error("Error processing response");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
 
   // Identify which content to show
   let content = <p>No movies to show...</p>;
@@ -57,6 +92,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
