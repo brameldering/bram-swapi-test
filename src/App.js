fb95e7c34b11a0aec7100855fb47755238e8d47a
brameldering <bram.eldering@gmail.com>
2023-06-09 11:03:07 +0200

    Spinning Loading Indicator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
+import LoadingIndicator from "./components/UI/LoadingIndicator";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-
-  // function fetchMoviesHandler() {
-  //   fetch("https://swapi.dev/api/films/")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
-  // }
+  const [isLoading, setIsloading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsloading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -37,6 +22,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsloading(false);
   }
 
   return (
@@ -45,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <LoadingIndicator segmentWidth={10} segmentLength={20} spacing={10} />}
       </section>
     </React.Fragment>
   );
